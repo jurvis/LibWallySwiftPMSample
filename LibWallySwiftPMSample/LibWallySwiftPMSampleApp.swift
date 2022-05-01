4f0607844062d587b4d58b9486d76570f00facdc
Jurvis Tan <5944973+jurvis@users.noreply.github.com>
2022-04-30 18:04:31 -0700

    Initial Commit

---
+++ LibWallySwiftPMSample/LibWallySwiftPMSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibWallySwiftPMSampleApp.swift
+//  LibWallySwiftPMSample
+//
+//  Created by Jurvis on 4/30/22.
+//
+
+import SwiftUI
+
+@main
+struct LibWallySwiftPMSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
